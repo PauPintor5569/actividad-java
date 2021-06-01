300dccd95d01c11e02fc498689a7c14c2c0458ff
Paula <pcpinto@uc.cl>
2021-05-31 21:52:22 -0400

    actividad frutas

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var manzanaraul = 4;
+var manzanapedro = 2;
+var frutilla = 4;
+var pera = 8;
+
+var total_manzanas = manzanaraul + manzanapedro;
+
+var frutillas_dos = frutilla / 2;
+
+var frutas = total_manzanas + frutilla + pera;
+
+
+document.write("<h4>Total de manzanas</h4>",total_manzanas);
+document.write("<h4>Frutillas para cada uno</h4>",frutillas_dos);
+document.write("<h4>Frutas en total</h4>",frutas);
